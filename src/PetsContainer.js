eb4a569796c8f9abda2c8a310e6835a4e2074a9f
Kevin Hu <pkvinhu@gmail.com>
2018-08-12 22:57:10 -0400

    pets container

---
+++ src/PetsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+default export class PetsContainer extends React.Component {
+  render() {
+  	return (
+      <div>
+        <h2>I have all the pets in me</h2>
+      </div>
+  	)
+  }
+}
